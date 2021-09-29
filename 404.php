62ae58c09affd5720a7ad6a3233e3289235042be
Manu Andino <manuuandinodev@gmail.com>
2021-09-29 15:05:35 -0300

    .

---
+++ 404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property=" og:title " content=" Dynamis 3D-Estudio de arquitectura y urbanismo ">
+    <meta property=" og:image " content=" https://i.ibb.co/GscsSDR/DYNAMIS-CUADRADO-BLANCO-para-wev.png ">
+    <meta property=" og:description " content=" Somos un estudio joven que se dedica a la producción de renders y visualización en el campo de la comunicación visual y la publicidad para estudios de arquitectura, desarrolladores inmobiliarios y campañas publicitarias. ">
+    <meta proprty=" og:author "  content="Manuel Andino">
+    <link rel="shortcut icon" href="icono/Dynamis-3D.ico" type="image/x-icon">
+    <!--Link de Font Awesome-->
+    <script src="https://kit.fontawesome.com/006325ae57.js" crossorigin="anonymous"></script>
+    <meta name="description" content="Somos un estudio joven que se dedica a la producción de renders y visualización en el campo de la comunicación visual y la publicidad para estudios de arquitectura, desarrolladores inmobiliarios y campañas publicitarias.">
+    <meta name="keywords" content="Estudio, arquitectura, urbanismo, modelado , renderizado, 3D, producción, audio-visual, realidad virtual, videos, inmobiliario, proyectos, servicios, calidad, ">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/Style.css">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" /> 
+    <title>Error 404</title>
+</head>
+<body id="body">
+    <section class="error">
+        <div class="image" >
+            <img src="https://i.ibb.co/Sy0wP66/67748194-717966101994719-6178594953929687040-n-removebg-preview.png" alt="">
+        </div>
+        <div class="container-error">
+            <h1 class="h1">Error 404</h1>
+            <p class="p">
+                Uups... Pagina no encontrada
+            </p>
+        </div>
+        <div class="home">
+            <a href="index.html">Volver a la Pagina Principal</a>
+        </div>
+    </section>
+</body>
+</html>
