118aa4256ee0c5e245c633671ce339f0e3ea2b06
Manu Andino <manuuandinodev@gmail.com>
2021-09-29 21:51:03 -0300

    Arregle una linea del archivo 404

---
+++ 404.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property=" og:title " content=" Dynamis 3D-Estudio de arquitectura y urbanismo ">
+    <meta property=" og:image " content=" https://i.ibb.co/GscsSDR/DYNAMIS-CUADRADO-BLANCO-para-wev.png ">
+    <meta property=" og:description " content=" Somos un estudio joven que se dedica a la producción de renders y visualización en el campo de la comunicación visual y la publicidad para estudios de arquitectura, desarrolladores inmobiliarios y campañas publicitarias. ">
+    <meta proprty=" og:author "  content="Manuel Andino">
+    <link rel="shortcut icon" href="icono/Dynamis-3D.ico" type="image/x-icon">
+    <!--Link de Font Awesome-->
+    <script src="https://kit.fontawesome.com/006325ae57.js" crossorigin="anonymous"></script>
+    <meta name="description" content="Somos un estudio joven que se dedica a la producción de renders y visualización en el campo de la comunicación visual y la publicidad para estudios de arquitectura, desarrolladores inmobiliarios y campañas publicitarias.">
+    <meta name="keywords" content="Estudio, arquitectura, urbanismo, modelado , renderizado, 3D, producción, audio-visual, realidad virtual, videos, inmobiliario, proyectos, servicios, calidad, ">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/Style.css">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" /> 
+    <title>Error 404</title>
+</head>
+<body id="body">
+    <section class="error" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+        <div class="image" >
+            <img src="https://i.ibb.co/Sy0wP66/67748194-717966101994719-6178594953929687040-n-removebg-preview.png" alt="Logo de Dynamis 3D">
+        </div>
+        <div class="container-error">
+            <h1 class="h1">Error 404</h1>
+            <p class="p">
+                Uups... Pagina no encontrada
+            </p>
+        </div>
+        <div class="home">
+            <a class="a" href="index.html">Volver a la Pagina Principal</a>
+        </div>
+    </section>
+    <!--AOS-->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>            
+    <!--Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+</body>
+</html>
